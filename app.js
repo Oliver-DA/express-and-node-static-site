b3d6d7a14db6883439e36ea569576cb296214ed3
Oliver-DA <71343538+Oliver-DA@users.noreply.github.com>
2020-11-14 21:24:22 -0500

    none

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { notFoundError, globalError } = require("./errorHandlers");
 
 //Initializing express app
 const app = express();
+const port = process.env.PORT || 5000;
 
 //Setting up pug as a view engine
 app.set("view engine", "pug");
@@ -21,5 +22,5 @@ app.use(notFoundError);
 app.use(globalError);
 
 //App listenning on port 3000
-app.listen(3000, () => console.log("Server up and running on localhost:3000"))
+app.listen(port, () => console.log("Server up and running on port" + port))
 
